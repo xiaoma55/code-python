133208bdc93acf9b7c787b132e2004acf21e0c2d
xiaoma <xiao.ma>
2021-08-09 14:23:04 +0800

    一键批量打开应用

---
+++ StartApplication/com.ma.startapplication/StartApplication.py
@@ -0,0 +1,92 @@
+import os
+
+
+def open_app(app_dir):
+    os.startfile(app_dir)
+
+
+if __name__ == '__main__':
+    # 打开Chrome
+    app_dir = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+    open_app(app_dir)
+
+    # 打开IDEA
+    app_dir = r'D:\application\IntelliJ IDEA 2020.2.3\bin\idea64.exe'
+    open_app(app_dir)
+
+    # 打开WebStorm
+    app_dir = r'D:\application\WebStorm 2020.3\bin\webstorm64.exe'
+    open_app(app_dir)
+
+    # 打开PyCharm
+    app_dir = r'D:\application\PyCharm 2020.2.3\bin\pycharm64.exe'
+    open_app(app_dir)
+
+    # 打开DataGrip
+    app_dir = r'D:\application\DataGrip 2020.2.3\bin\datagrip64.exe'
+    open_app(app_dir)
+
+    # 打开XShell
+    app_dir = r'D:\application\Xshell6 Plus\Xshell.exe'
+    open_app(app_dir)
+
+    # 打开PostMan
+    app_dir = r'C:\Users\V\AppData\Local\Postman\Postman.exe'
+    open_app(app_dir)
+
+    # 打开VMware
+    app_dir = r'D:\application\VMware\vmware.exe'
+    open_app(app_dir)
+
+    # 打开Panda VPN
+    app_dir = r'D:\soft\Panda\Panda.exe'
+    open_app(app_dir)
+
+    # 打开Wox
+    app_dir = r'C:\Users\V\AppData\Local\Wox\Wox.exe'
+    open_app(app_dir)
+
+    # 打开EveryThing
+    app_dir = r'C:\Program Files\Everything\Everything.exe'
+    open_app(app_dir)
+
+    # ==========================================================================================================
+
+    # 打开微信
+    app_dir = r'D:\soft\WeChat\WeChat.exe'
+    open_app(app_dir)
+
+    # 打开网易邮箱
+    app_dir = r'D:\soft\wangYiMail\MailMaster\Application\mailmaster.exe'
+    open_app(app_dir)
+
+    # 打开有道云笔记
+    app_dir = r'D:\soft\YoudaoNote\YoudaoNote.exe'
+    open_app(app_dir)
+
+    # 打开钉钉
+    app_dir = r'D:\soft\DingDing\DingtalkLauncher.exe'
+    open_app(app_dir)
+
+    # 打开网易有道词典
+    app_dir = r'D:\soft\YouDaoCiDian\Dict\YoudaoDict.exe'
+    open_app(app_dir)
+
+    # 打开FSCapture
+    app_dir = r'D:\soft\FSCapture\FSCapture.exe'
+    open_app(app_dir)
+
+    # 打开Snipaste贴图
+    app_dir = r'D:\soft\神奇的截图工具Snipaste-1.11.0-x86\Snipaste-1.11.0-x86\Snipaste.exe'
+    open_app(app_dir)
+
+    # 打开天若OCR
+    app_dir = r'D:\soft\OCR\天若OCR文字识别V3.0.exe'
+    open_app(app_dir)
+
+    # 打开TrafficMonitor
+    app_dir = r'D:\soft\TrafficMonitor\TrafficMonitor.exe'
+    open_app(app_dir)
+
+
+
